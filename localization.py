90ce25d02efd21664ba3d36e010e09609661f9f4
cooliceman <robotinthesun@gmail.com>
2016-11-19 15:53:03 +0800

    improve perfomance

--- localization.py
+++ localization.py
@@ -73,10 +73,12 @@ def combine_xml(src_folder, dest_folder, force_update):
     src_file_list = find_string_resource_files(src_folder)
     if not src_file_list:
         print "No resource file found in source folder"
+        return
     # find string resource file in destination folder
     dest_file_list = find_string_resource_files(dest_folder)
-    if not src_file_list:
+    if not dest_file_list:
         print "No resource file found in destination folder"
+        return
     for src_file in src_file_list:
         values_folder = os.path.split(os.path.dirname(src_file))[1]
         target_file = find_string_resource_file_of(dest_file_list, values_folder)
@@ -85,6 +87,7 @@ def combine_xml(src_folder, dest_folder, force_update):
         print "start combine:%s and %s" %(src_file, target_file)
         src_document = load_xml(src_file)
         dest_document = load_xml(target_file)
+        rewrite = False
         # iterator the source xml document element and update the destination xml document
         for entity in src_document.findall("string"):
             key = entity.get("name")
@@ -92,13 +95,18 @@ def combine_xml(src_folder, dest_folder, force_update):
             if not key:
                 print "Warning:no key found!!!!"
                 continue
+            else:
+                key = key.strip()
             if not value:
                 print "no value found, skip"
                 continue
+            else:
+                value = value.strip()
             if exist(dest_document, key):
                 if force_update:
                     print "Key already exist, update"
                     dest_document.find("string[@name='%s']" % key).text = value
+                    rewrite = True
                     continue
                 else:
                     print "I:%s already exist, skip" % key
@@ -108,7 +116,9 @@ def combine_xml(src_folder, dest_folder, force_update):
             localize_element.text = value
             # 追加结点
             dest_document.getroot().append(localize_element)
-        dest_document.write(target_file, xml_declaration=True, encoding='UTF-8', pretty_print=True)
+            rewrite = True
+        if rewrite:
+            dest_document.write(target_file, xml_declaration=True, encoding='UTF-8', pretty_print=True)
 
 
 def localize(input_file, dest_dir, force_update):
@@ -117,34 +127,49 @@ def localize(input_file, dest_dir, force_update):
         wb = openpyxl.load_workbook(input_file)
     except:
         print "Excel file can't be open"
+        return
     # 获取默认Sheet
     ws = wb.active
     # 获取字符串资源文件列表
     file_list = find_string_resource_files(dest_dir)
+    if not file_list:
+        print "No resource file found in destination folder"
+        return
     # 遍历Excel每个语言数据行
     for col in range(3, ws.max_column + 1):
         # 提取语言码
         lang_code = ws.cell(row=1, column=col).value
-        print "\n%s" % lang_code
+        if not lang_code:
+            print "no language code, skip"
+            continue
+        else:
+            lang_code = lang_code.strip()
         # 获取对应语言的资源文件
-        lang_string_file = find_string_resource_file_of(file_list, lang_code)
+        lang_string_file = find_string_resource_file_of(file_list, "values-" + lang_code)
         if not lang_string_file:
             continue
         string_map = load_xml(lang_string_file)
+        print "\n%s %s" % (lang_code, lang_string_file)
+        rewrite = False #标识文件是否有变更
         for r in range(4, ws.max_row + 1):
             key = ws.cell(row=r, column=1).value
             value = ws.cell(row=r, column=col).value
-            print key, lang_code, value
+            print key, ":", value
             if not key:
                 print "no key exist at:%d,%d" % (r, col)
                 break
+            else:
+                key = key.strip()
             if not value:
                 print "no value found, skip"
                 continue
+            else:
+                value = value.strip()
             if exist(string_map, key):
                 if force_update:
                     print "Key already exist, update"
                     string_map.find("string[@name='%s']" % key).text = value
+                    rewrite = True
                     continue
                 else:
                     print "Warning:%s already exist, skip" % key
@@ -154,7 +179,9 @@ def localize(input_file, dest_dir, force_update):
             localize_element.text = value
             # 追加结点
             string_map.getroot().append(localize_element)
-        string_map.write(lang_string_file, xml_declaration=True, encoding='UTF-8', pretty_print=True)
+            rewrite = True
+        if rewrite:
+            string_map.write(lang_string_file, xml_declaration=True, encoding='UTF-8', pretty_print=True)
 
 
 def find_string_resource_files(folder):
@@ -176,13 +203,13 @@ def find_string_resource_files(folder):
     return file_list
 
 
-def find_string_resource_file_of(file_list, language_code):
-    """find_string_resource_file_of(file_list, language_code)
+def find_string_resource_file_of(file_list, values_dir_name):
+    """find_string_resource_file_of(file_list, values_dir_name)
 
-    通过语言码来找到对应的string.xml文件路径
+    通过语言码对应文件夹名称来找到对应的string.xml文件路径
     """
     for f in file_list:
-        if os.path.dirname(f).endswith(language_code):
+        if os.path.dirname(f).endswith(values_dir_name):
             return f
 
 
